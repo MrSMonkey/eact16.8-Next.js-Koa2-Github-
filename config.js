c10b65809b00a50b214332686a1243044e3b27e5
李春霖 <chunlin.li7@mycos.com>
2021-11-23 17:40:55 +0800

    6-1_6-7  学习代码提交

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  github: {
+    client_id: 'de1bcf4dd9fac6127048',
+    client_secret: '759634e08e3fd5a726e3e189831f40c2a966bf0c',
+  },
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const next = require('next');
+const session = require('koa-session');
 
 const dev = process.env.NODE_ENV !== 'produciton';
 const app = next({ dev });
@@ -9,8 +10,29 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
+  server.keys = ['jocker app']
+  const SESSION_CONFIG = {
+    key: 'jid',
+    // store: {}
+  }
+  server.use(session(SESSION_CONFIG, server))
+  server.use(async (ctx, next) => {
+    // console.log('ctx.session.user', ctx.session || ctx.session.user);
+
+    // ctx.session = ctx.session || {};
+    // if(!ctx.session.user) {
+    //   ctx.session.user = {
+    //     userName: 'jocker',
+    //     age: 18
+    //   }
+    // } else {
+      console.log(`session is:`, ctx.session);
+    // }
+
+    await next();
+  })
   router.get('/a/:id', async (ctx, next) => {
-    console.log('ctx.params.id', ctx.params.id);
+    // console.log('ctx.params.id', ctx.params.id);
     const id= ctx.params.id;
     await handle(ctx.req, ctx.res, {
       pathname: '/a',
@@ -18,10 +40,23 @@ app.prepare().then(() => {
     });
     ctx.respond = false;
   })
+  router.get('/set/user', async (ctx, next) => {
+    // ctx.session = {}
+    ctx.session.user = {
+      userName: 'jocker',
+      age: 18
+    }
+    ctx.body = 'set session successed'
+  })
   server.use(router.routes())
   server.use(async (ctx) => {
+    ctx.cookies.set('id', 'user:xxxxx');
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
+  })
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200
+    await next()
   })
   server.listen(3000, () => console.log('server successed'));
 })
